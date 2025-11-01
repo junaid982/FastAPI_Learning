ec319dc2533533627d4a9f4690739c5ff0f6c58c
Junaid Ansari <ansarijunaid957@gmail.com>
2025-11-01 23:58:56 +0530

    working on how to render html

---
+++ 14_render_html/main.py
@@ -0,0 +1,18 @@
+
+from fastapi import FastAPI
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+
+
+app = FastAPI(name = "render_templates")
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/")
+async def index_view(request):
+    """
+    This method is used to render html file 
+    """
+    
+    return templates.TemplateResponse("index.html" , {"request" : request})
